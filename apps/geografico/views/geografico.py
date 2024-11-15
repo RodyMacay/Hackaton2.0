a59e91824e9460cc34b42756739a699f28e4b3c7
CristhianLaid <cmurillos@unemi.edu.ec>
2024-11-15 08:13:31 -0500

    mejorando templete

--- apps/geografico/views/geografico.py
+++ apps/geografico/views/geografico.py
@@ -5,7 +5,7 @@ from django.conf import settings
 import os
 
 class BiodiversityMapView(TemplateView):
-    template_name = "geografico/biodiversity_map.html"
+    template_name = "app/geografico/biodiversity_map.html"
 
     def get_context_data(self, **kwargs):
         context = super().get_context_data(**kwargs)
